7c26a2aa81d6e2cc4678de92e2505a2f128756ae
shivam <000shiv2304@gmail.com>
2026-04-08 23:17:28 +0530

    fix bug

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
-//import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
+import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
 
 const Navbar = () => {
     const { user, logout } = useAuth();
@@ -20,7 +20,7 @@ const Navbar = () => {
                     onClick={logout}
                     className="flex items-center text-gray-600 hover:text-red-500 transition-colors"
                 >
-                    {/* <ArrowLeftOnRectangleIcon className="w-5 h-5 mr-1" /> */}
+                     <ArrowLeftOnRectangleIcon className="w-5 h-5 mr-1" /> 
                     <span className="text-sm font-medium">Logout</span>
                 </button>
             </div>
